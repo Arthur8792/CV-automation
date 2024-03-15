e9e7372556628b2d9db2e277af9f9fb5bf5ab232
Arthur BAUDOT <abaudot@polarysgroup.com>
2024-03-15 18:11:23 +0100

    Langchain model

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CV-automation
+
+To run, just enter : ```python main.py```
---
+++ main.py
@@ -0,0 +1,46 @@
+from langchain_openai import ChatOpenAI
+from PyPDF2 import PdfReader 
+
+OPENAI_MODEL = 'gpt-3.5-turbo'
+OPENAI_API_KEY = 'Enter API key'
+
+def pdf_to_txt(pdf_file: str)->None:
+    """
+    Converts a pdf file to a txt file.
+
+    Args:
+        pdf_file : path of the PDF file.
+    """
+    reader = PdfReader(pdf_file)
+    with open("result.txt","w",encoding='utf-8') as file:
+        for page_num in range(len(reader.pages)):
+            page = reader.pages[page_num]
+            extracted_text = page.extract_text()
+            file.write(extracted_text)
+
+def ask_question(model: str, api_key: str, question: str)->str:
+    """
+    Creates the langchain LLM model using the OpenAI API key.
+
+    Args:
+        model: the LLM to use
+        api_key : the OpenAI API key
+        question : the question to ask
+    Returns:
+        The response to the question.
+    """
+    llm = ChatOpenAI(model=model, openai_api_key = api_key)
+    response = llm.invoke(question)
+    return response.content
+
+def main():
+    # Entry PDF file
+    pdf_file = 'original-cv/Exemple1.pdf'
+    # Convert from PDF to txt
+    pdf_to_txt(pdf_file)
+    question = "Introduce yourself"
+    response = ask_question(model=OPENAI_MODEL, api_key=OPENAI_API_KEY, question=question)
+    print(response)
+
+if __name__ == '__main__':
+    main()
