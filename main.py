88b0819e0452abf61f734659065ec2b43324de6d
Arthur BAUDOT <abaudot@polarysgroup.com>
2024-03-17 18:45:52 +0100

    1st pipeline from pdf to formatted content with 2 toy prompts

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain
 from langchain_openai import ChatOpenAI
 from PyPDF2 import PdfReader 
 
@@ -16,35 +18,62 @@ def pdf_to_txt(pdf_file: str)->None:
         pdf_file : path of the PDF file.
     """
     reader = PdfReader(pdf_file)
-    with open("result.txt","w",encoding='utf-8') as file:
+    with open("cv_content.txt","w",encoding='utf-8') as file:
         for page_num in range(len(reader.pages)):
             page = reader.pages[page_num]
             extracted_text = page.extract_text()
             file.write(extracted_text)
 
-def ask_question(model: str, api_key: str, question: str)->str:
-    """
-    Creates the langchain LLM model using the OpenAI API key.
-
-    Args:
-        model: the LLM to use
-        api_key : the OpenAI API key
-        question : the question to ask
-    Returns:
-        The response to the question.
-    """
-    llm = ChatOpenAI(model=model, openai_api_key = api_key)
-    response = llm.invoke(question)
-    return response.content
-
 def main():
+
     # Entry PDF file
     pdf_file = 'original-cv/Exemple1.pdf'
+
     # Convert from PDF to txt
     pdf_to_txt(pdf_file)
-    question = "Introduce yourself"
-    response = ask_question(model=OPENAI_MODEL, api_key=OPENAI_API_KEY, question=question)
-    print(response)
+
+    # Extract text contents of the CV
+    cv_file = open("cv_content.txt","r")
+    cv_content = cv_file.read()
+
+    ## LLM PART
+
+    # Create LLM model
+    llm = ChatOpenAI(model=OPENAI_MODEL, api_key=OPENAI_API_KEY)
+
+    # Create prompts
+    prompt1 = PromptTemplate(
+        input_variables=['CV'],
+        template = "Donne-moi la formation la plus récente du CV suivant : {CV}"
+    )
+
+    prompt2 = PromptTemplate(
+        input_variables=['CV'],
+        template = "Liste-moi ses compétences techniques, en détaillant les langages, les outils collaboratifs, et les outils métiers du CV suivant : {CV}"
+    )
+
+    # Create chains : 
+    chain1 = LLMChain(llm = llm, prompt=prompt1, output_key="derniere-formation")
+    chain2 = LLMChain(llm = llm, prompt=prompt2, output_key="competences-techniques")
+
+    # Create sequence of these chains :
+    sequential_chain = SequentialChain(
+        chains = [chain1, chain2],
+        input_variables = ["CV","CV"],
+        output_variables = ["derniere-formation","competences-techniques"]
+    )
+
+    result = sequential_chain.invoke({"CV":cv_content, "CV":cv_content})
+
+    ## END LLM PART
+
+    # Close CV file : 
+    cv_file.close()
+
+    # Write result in TXT file : 
+    with open("result.txt",'w', encoding='utf-8') as result_file:
+        result_file.write("Dernière formation : " + result["derniere-formation"] + "\n")
+        result_file.write("Compétences techniques : " + result["competences-techniques"] + "\n")
 
 if __name__ == '__main__':
     main()
